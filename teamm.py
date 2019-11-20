7e1cec650374cc8a2068770e4f4ee28f4ef70d06
Justin Riley <20hz20khz@gmail.com>
2019-11-20 00:40:22 -0500

    Create teamm.py

---
+++ teamm.py
@@ -0,0 +1,14 @@
+from turtle import *
+m = Turtle()
+colormode(255)
+
+def reminder():
+  # by Mr. Riley
+  # don't forget to put a comment with your name at the top of each function you make
+  # this will help to keep track of who created each function
+  return True
+  
+
+
+
+update()
